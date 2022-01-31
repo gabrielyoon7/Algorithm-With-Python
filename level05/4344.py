1ede7b4de6c1d8475854889e6ea99b9b66f7f84d
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-02-01 00:29:36 +0900

    lv5 끝

--- level05/4344.py
+++ level05/4344.py
@@ -1,1 +1,14 @@
 #평균은 넘겠지
+C=int(input())
+for c in range(C):
+    students = input().split(" ")
+    sum_score=0
+    for i in range(1,int(students[0])+1):
+        sum_score += int(students[i])
+    average=sum_score/int(students[0])
+    count=0
+    for i in range(1,int(students[0])+1):
+        if average < int(students[i]):
+            count+=1
+    percentage = round(count/int(students[0])*100, 3)
+    print(format(percentage, '.3f')+"%")
