e0654b92ce2ca1eecb6dc63831f2415a331f4a8d
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-02-08 23:19:52 +0900

    2775

--- level08/2775.py
+++ level08/2775.py
@@ -1,10 +1,21 @@
 #부녀회장이 될테야
 T=int(input())
 for i in range(T):
-    k=int(input())
-    n=int(input())
-    if k==0:
-        print(n)
-        break
-    else:
-        pass
+    K=int(input())
+    N=int(input())
+    temp=[]
+    for j in range(1,N+1):
+        temp.append(j)
+    # print(temp)
+    for j in range(K):
+        floor=[]
+        for k in range(1,N+1):
+            tmp=0
+            for l in range(k):
+                tmp+=temp[l]
+            floor.append(tmp)
+        # print(floor)
+        temp=list(floor)
+    print(floor[-1])
+
+    
