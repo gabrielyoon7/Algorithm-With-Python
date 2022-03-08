526fcb8ad1370280b6a94bf522415b03ea6586d3
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-03-08 16:16:35 +0900

    dd

--- level12/2750.py
+++ level12/2750.py
@@ -1,7 +1,13 @@
 #수 정렬하기
 N=int(input())
 num=[]
-selectionSort=[]
 for i in range(N):
     num.append(int(input()))
-print(num)
+for i in range(N):
+    min_index=i
+    for j in range(i+1, N):
+        if num[min_index]>num[j]:
+            min_index=j
+    num[i], num[min_index] = num[min_index], num[i]
+for i in num:
+    print(i)
